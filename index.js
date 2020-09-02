6352c4685b52cb3c5845fb57b47689b08a2aeef1
Collbrothers <aron@kyleback.com>
2020-09-02 19:16:38 +0200

    Changed colors, added debug and warn.

--- index.js
+++ index.js
@@ -4,13 +4,34 @@ function base() {
     return `${config.White}[${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}:${date.getMinutes()}]${config.Reset}`
 }
 module.exports = {
+    /**
+     * @param {string} message The message you want to display
+     */
     error(message) {
-        console.log(base() + `${config.Bold}${config.Red} <ERROR>${config.Reset} — ${config.White}${message}${config.Reset}`)
+        console.log(base() + `${config.Bold}${config.Red} <Error> — ${message}${config.Reset}`)
     },
+    /**
+     * @param {string} message The message you want to display
+     */
     info(message) {
-        console.log(base() + `${config.Bold}${config.Yellow} <DEBUG>${config.Reset} — ${config.White}${message}${config.Reset}`)
+        console.log(base() + `${config.Bold}${config.Blue} <Info> — ${message}${config.Reset}`)
     },
+    /**
+     * @param {string} message The message you want to display
+     */
     success(message) {
-        console.log(base() + `${config.Bold}${config.Green} <SUCCESS>${config.Reset} — ${config.White}${message}${config.Reset}`)
+        console.log(base() + `${config.Bold}${config.Green} <Success> — ${message}${config.Reset}`)
+    },
+    /**
+     * @param {string} message The message you want to display
+     */
+    debug(message) {
+        console.log(base() + `${config.Bold}${config.Magenta} <Debug> — ${message}${config.Reset}`)
+    },
+    /**
+     * @param {string} message The message you want to display
+     */
+    warn(message) {
+        console.log(base() + `${config.Bold}${config.Yellow} <Warn> — ${message}${config.Reset}`)
     }
 }
--- readme.md
+++ readme.md
@@ -4,10 +4,13 @@
 ```js
 // Import package
 const logger = require("sign-logger");
-logger.error("Hello world!") // [2020-9-2-16:35] <ERROR> — Hello world!
-logger.info("Hmm, something cool is happening here!") // [2020-9-2-16:47] <DEBUG> — Hmm, something cool is happening here!
-logger.success("Yay! Something went better than expected!") // [2020-9-2-16:49]  <SUCCESS> — Yay! Something went better than expected! 
+logger.error("Hello world!") // [2020-9-2-16:35] <Error> — Hello world!
+logger.info("Hmm, something cool is happening here!") // [2020-9-2-16:35] <Info> — Hmm, something cool is happening here!
+logger.success("Yay! Something went better than expected!") // [2020-9-2-16:35]  <Success> — Yay! Something went better than expected! 
+logger.warn("Oh no! Something went wrong, but not fatal!") // [2020-9-2-16:35]  <Warn> — Oh no! Something went wrong, but not fatal!
+logger.debug("Hmm, Someone needs to check this line of code out!") // [2020-9-2-16:35]  <Debug> — Hmm, Someone needsto check this line of code out!
 ``` 
-
+### Changelog
+* Added debug() and warn(), Also fixed colors
 ### Author:
 ##### [Collbrothers](https://github.com/Collbrothers)
